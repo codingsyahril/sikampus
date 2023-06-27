2d024082df109377d24b178cce4ba12612c521f8
codingsyahril <135492621+codingsyahril@users.noreply.github.com>
2023-06-27 20:11:31 +0800

    accaja

--- conf/autentikasi.php
+++ conf/autentikasi.php
@@ -1,9 +1,13 @@
 <?php
+include ('config.php');
 $username =$_POST['username'];
 $password =$_POST['password'];
 
-echo $username;
-
-// sandi
-echo $password;
+$query = mysqli_query($koneksi,"SELECT * FROM tb_users WHERE username='$username' AND password='$password'");
+if(mysqli_num_rows($query)==1){
+header('location:../app');
+}
+else{
+    header('location:../index.php?error=1');
+    }
 ?>
